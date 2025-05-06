73fcff6951519e6125bdbcb1e42648967afc0ea7
Ale Pereira <naitwa-alexandre@gmail.com>
2025-05-06 13:46:39 -0300

    finished section

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 // Controls player movement and rotation.
 public class Collectible : MonoBehaviour
 {
   public float rotationSpeed = 0.5f;
+  public GameObject onCollectEffect;
 
   private Rigidbody rb;
-
 
   private void Start()
   {
@@ -17,6 +19,17 @@ public class Collectible : MonoBehaviour
   void Update()
   {
     transform.Rotate(0, rotationSpeed, 0);
+  }
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.CompareTag("Player"))
+    {
+      //Destroy the collectible
+      Destroy(gameObject);
+
+      //Instantiate the particle effect
+      Instantiate(onCollectEffect, transform.position, transform.rotation);
+    }
   }
 
 
